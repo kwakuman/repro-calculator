500e0d4d6096519a4da03bd6f32f7763c19941e6
kwakuman <kwakuman@pm.me>
2020-11-19 14:02:18 +0100

    Stažení dat a výpočet reprodukčního čísla R je hotov. Zveřejnění zprávy na twitter je připraveno. Zbývá vyřešit jak se přihlásit k Twitter API, respektive kde uchovávat údaje k přihlašování a jak je načíst.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # repro-calculator
-Výpočet reprodukčního čísla pro Českou republiku z dat zveřejňovaných ministerstvem zdravotnictví
+Výpočet reprodukčního čísla pro Českou republiku z dat zveřejňovaných ministerstvem zdravotnictví.
 Způsob výpočtu je převzat z metodiky ministerstva (viz. 04-monitoring-reprodukcniho-cisla.pdf, str. 3)
---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import datetime
+import tweepy
+
+if __name__ == '__main__':
+    # stáhni data z ministerstva
+    url = 'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakaza.json'
+    res = requests.get(url)
+    data = res.json()
+    # připrav datumy, z kterých se bude vypočítávat reprodukční číslo
+    dnes = datetime.datetime.now()
+    datumyCitatel = [(dnes - datetime.timedelta(days=x)).strftime('%Y-%m-%d') for x in range(1, 8)]
+    datumyJmenovatel = [(dnes - datetime.timedelta(days=x)).strftime('%Y-%m-%d') for x in range(6, 13)]
+    # vypočítej reprodukční číslo
+    reprodukcniCislo = int()
+    citatel = sum(
+        [x['prirustkovy_pocet_nakazenych'] for x in data['data'] if x['datum'] in datumyCitatel
+    ])
+    jmenovatel = sum(
+        [x['prirustkovy_pocet_nakazenych'] for x in data['data'] if x['datum'] in datumyJmenovatel
+    ])
+    reprodukcniCislo = citatel/jmenovatel
+    #TODO přihlaš se na Twitter
+    CONSUMER_KEY =
+    CONSUMER_SECRET =
+    ACCESS_TOKEN =
+    ACCESS_TOKEN_SECRET =
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # Create API object
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    #tweetni reprodukční číslo
+    api.update_status(f'Dnes je {dnes.strftime("%d.%m.%Y")} a reprodukční číslo je {reprodukcniCislo:.2f}')
